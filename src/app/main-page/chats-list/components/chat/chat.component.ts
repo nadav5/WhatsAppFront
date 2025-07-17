aeb6238511aa29d5ccd221939c6abcdc06d27808
nadav5 <nadavbar205@gmail.com>
2025-07-17 15:05:18 +0300

    delete arrs
    from a lot of arr to one chat

--- src/app/main-page/chats-list/components/chat/chat.component.ts
+++ src/app/main-page/chats-list/components/chat/chat.component.ts
@@ -17,19 +17,20 @@ export class ChatComponent implements OnInit {
   public userName!: string;
   public messages: MessagesDto[] = [];
 
-
-  public chatName!: string;
-  public chatId!: string;
-  public isGroup!: boolean;
   public activeUsers: ActiveUserDto[] = [];
 
-  
   public showOptionsMenu: boolean = false;
 
   public showDescriptionPopup: boolean = false;
-  public groupDescription: string = '';
 
-  public chat!:Chat;
+  public chat: Chat = {
+    _id: '',
+    name: '',
+    isGroup: false,
+    members: [],
+    createdAt: '',
+    description: '',
+  };
   constructor(
     private route: ActivatedRoute,
     private apiService: ApiService,
@@ -37,23 +38,23 @@ export class ChatComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.chatId = this.route.snapshot.paramMap.get('id')!;
+    this.chat._id = this.route.snapshot.paramMap.get('id')!;
     this.userName = localStorage.getItem(STORAGE_KEYS.LOGGED_USER)!;
-    this.socketService.joinChat(this.chatId);
+    this.socketService.joinChat(this.chat._id);
 
-    this.apiService.getChatById(this.chatId).subscribe((res: Chat) => {
-      this.chat=res;
-      this.isGroup = res.isGroup;
+    this.apiService.getChatById(this.chat._id).subscribe((res: Chat) => {
+      this.chat = res;
+      this.chat.isGroup = res.isGroup;
       if (res.isGroup && res.name) {
-        this.chatName = res.name;
+        this.chat.name = res.name;
       } else {
         const otherMember = res.members.find((m) => m !== this.userName);
-        this.chatName = otherMember ? otherMember : 'Private Chat';
+        this.chat.name = otherMember ? otherMember : 'Private Chat';
       }
     });
 
     this.apiService
-      .getMessagesByChatId(this.chatId)
+      .getMessagesByChatId(this.chat._id)
       .subscribe((res: MessagesDto[]) => {
         this.messages = res.map((msg) => ({
           _id: msg._id,
@@ -68,7 +69,7 @@ export class ChatComponent implements OnInit {
       });
 
     this.socketService.onNewMessage((msg) => {
-      if (msg.chatId === this.chatId) {
+      if (msg.chat.id === this.chat._id) {
         this.messages.push({
           _id: msg.id,
           sender: msg.sender,
@@ -86,12 +87,12 @@ export class ChatComponent implements OnInit {
   public sendMessage(): void {
     if (this.newMessage.trim()) {
       this.apiService
-        .createMessage(this.chatId, this.userName, this.newMessage)
+        .createMessage(this.chat._id, this.userName, this.newMessage)
         .subscribe({
           next: (res) => {
             this.socketService.sendMessage({
               id: res._id,
-              chatId: this.chatId,
+              chatId: this.chat._id,
               sender: res.sender,
               text: res.content,
               time: res.timestamp,
@@ -115,12 +116,9 @@ export class ChatComponent implements OnInit {
 
   public removeUser() {}
 
-  public addUser() {
-   
-  }
+  public addUser() {}
 
-  public showGroupDescription():void {
-    this.groupDescription = this.chat.description;
+  public showGroupDescription(): void {
     this.showDescriptionPopup = true;
   }
 }
