d40474d80f8d95ad3aa37caef949e223a461105e
nadav5 <nadavbar205@gmail.com>
2025-07-20 16:10:08 +0300

    chack user local storage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './home/page-not-found/page-not-found.component';
+import { AuthGuard } from './main-page/guard/auth.guard';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
-  { path: 'chats', loadChildren: () => import('./main-page/main-page.module').then(m => m.MainPageModule) },
+  { path: 'chats', loadChildren: () => import('./main-page/main-page.module').then(m => m.MainPageModule),  canActivate: [AuthGuard] },
   { path: '**', component: PageNotFoundComponent }
 ];
 
---
+++ src/app/main-page/guard/auth.guard.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { STORAGE_KEYS } from '../chats-list/constants';
+import { ApiService } from '../service/api.service';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router, private apiService: ApiService) {}
+
+  canActivate(): Observable<boolean> {
+    const token = localStorage.getItem(STORAGE_KEYS.LOGGED_USER);
+    if (!token) {
+      this.router.navigate(['/login']);
+      return of(false);
+    }
+
+    return new Observable<boolean>((observer) => {
+      this.apiService.getUserByUserName(token).subscribe({
+        next: (u) => {
+          observer.next(true);
+          observer.complete();
+        },
+        error: (err) => {
+          this.router.navigate(['/login']);
+          observer.next(false);
+          observer.complete();
+        },
+      });
+    });
+  }
+}
