e501c2e3b5071d869efe5efd368ca069dd7f6ec1
nadav5 <nadavbar205@gmail.com>
2025-07-22 13:40:40 +0300

    origin *

--- src/app/main-page/chats-list/constants.ts
+++ src/app/main-page/chats-list/constants.ts
@@ -1,5 +1,5 @@
 export const STORAGE_KEYS = {
   LOGGED_USER: 'loggedUser',
-  ANGULAR_PATH: 'http://localhost:3000',
+  ANGULAR_PATH: 'http://192.168.1.1:3000',
   // NESTJS_PATH: 'http://localhost:4000'
 };
