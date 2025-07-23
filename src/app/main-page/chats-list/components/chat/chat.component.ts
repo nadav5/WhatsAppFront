d8e8e9f50702af5ce19e4cc554458f42e8cc4573
nadav5 <nadavbar205@gmail.com>
2025-07-23 15:03:40 +0300

    close popup

--- src/app/main-page/chats-list/components/chat/chat.component.ts
+++ src/app/main-page/chats-list/components/chat/chat.component.ts
@@ -1,4 +1,10 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ApiService } from 'src/app/main-page/service/api.service';
 import { SocketService } from 'src/app/main-page/service/socket.service';
@@ -32,7 +38,8 @@ export class ChatComponent implements OnInit {
 
   @ViewChild('scrollAnchor') private scrollAnchor!: ElementRef;
 
-  
+  @ViewChild('optionsMenu') public optionsMenu!: ElementRef;
+
   public chat: Chat = {
     _id: '',
     name: '',
@@ -67,7 +74,9 @@ export class ChatComponent implements OnInit {
       if (res.isGroup && res.name) {
         this.chat.name = res.name;
       } else {
-        const other:string | undefined = res.members.find((m) => m !== this.userName);
+        const other: string | undefined = res.members.find(
+          (m) => m !== this.userName
+        );
         this.otherMember = other || '';
         this.chat.name = other || 'Private Chat';
         this.isOtherOnline = this.activeUsers.includes(this.otherMember);
@@ -76,7 +85,7 @@ export class ChatComponent implements OnInit {
       this.updateActiveUsersInChat();
     });
 
-    this.apiService.getMessagesByChatId(this.chat._id).subscribe((res ) => {
+    this.apiService.getMessagesByChatId(this.chat._id).subscribe((res) => {
       this.messages = res.map((msg: MessagesDto) => ({
         _id: msg._id,
         sender: msg.sender,
@@ -119,7 +128,7 @@ export class ChatComponent implements OnInit {
   private updateActiveUsersInChat(): void {
     if (this.chat.members) {
       console.log('here', this.chat.members);
-      this.activeUsersInChat = this.chat.members.filter((m:string) =>
+      this.activeUsersInChat = this.chat.members.filter((m: string) =>
         this.activeUsers.includes(m)
       );
     }
@@ -132,40 +141,40 @@ export class ChatComponent implements OnInit {
   }
 
   public sendMessage(): void {
-  if (this.newMessage.trim()) {
-    this.apiService
-      .createMessage(this.chat._id, this.userName, this.newMessage)
-      .subscribe({
-        next: (res: MessagesDto) => {
-          this.socketService.sendMessage({
-            id: res._id,
-            chatId: this.chat._id,
-            sender: res.sender,
-            text: res.content,
-            time: res.timestamp,
-          });
-          this.newMessage = '';
-        },
-        error: (err) => {
-          console.error('Error sending message:', err);
+    if (this.newMessage.trim()) {
+      this.apiService
+        .createMessage(this.chat._id, this.userName, this.newMessage)
+        .subscribe({
+          next: (res: MessagesDto) => {
+            this.socketService.sendMessage({
+              id: res._id,
+              chatId: this.chat._id,
+              sender: res.sender,
+              text: res.content,
+              time: res.timestamp,
+            });
+            this.newMessage = '';
+          },
+          error: (err) => {
+            console.error('Error sending message:', err);
 
-          if (err.status === 403) {
-            Swal.fire({
-              icon: 'error',
-              title: 'Access Denied',
-              text: 'You are not a member of this chat.',
-            });
-          } else {
-            Swal.fire({
-              icon: 'error',
-              title: 'Error',
-              text: 'There was a problem sending the message.',
-            });
-          }
-        },
-      });
+            if (err.status === 403) {
+              Swal.fire({
+                icon: 'error',
+                title: 'Access Denied',
+                text: 'You are not a member of this chat.',
+              });
+            } else {
+              Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'There was a problem sending the message.',
+              });
+            }
+          },
+        });
+    }
   }
-}
 
   public backToChatList(): void {
     this.socketService.leaveChat(this.chat._id);
@@ -266,17 +275,16 @@ export class ChatComponent implements OnInit {
   }
 
   public handleRemoveUser(userName: string): void {
-  this.apiService.removeMemberFromChat(this.chat._id, userName).subscribe({
-    next: (res) => {
-      this.chat = res;
-      this.refreshUsersLists();
+    this.apiService.removeMemberFromChat(this.chat._id, userName).subscribe({
+      next: (res) => {
+        this.chat = res;
+        this.refreshUsersLists();
 
-      this.socketService.leaveChatAsUser(this.chat._id, userName);
-    },
-    error: (err) => console.error('Error removing user from chat:', err),
-  });
-}
-
+        this.socketService.leaveChatAsUser(this.chat._id, userName);
+      },
+      error: (err) => console.error('Error removing user from chat:', err),
+    });
+  }
 
   public toShowAddUsrPopup(): void {
     this.refreshUsersLists();
@@ -287,6 +295,20 @@ export class ChatComponent implements OnInit {
     this.showDescriptionPopup = true;
   }
 
+  @HostListener('document:click', ['$event'])
+  public onDocumentClick(event: MouseEvent): void {
+    const target:HTMLElement  = event.target as HTMLElement;
 
-  
+    const clickedInsideMenu:boolean  = this.optionsMenu?.nativeElement.contains(target);
+    const clickedOnButton:Element  | null = target.closest('.chat-options');
+
+    if (!clickedInsideMenu && !clickedOnButton) {
+      this.showOptionsMenu = false;
+
+      this.showOptionsMenu = false;
+      this.showParticipantsPopup = false;
+      this.showAddUsrPopup = false;
+      this.showDescriptionPopup = false;
+    }
+  }
 }
